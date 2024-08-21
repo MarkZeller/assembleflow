3e0b92d21e4e880cf3064f71e9164174a319885d
evansawula <129804658+evansawula@users.noreply.github.com>
2024-08-21 14:36:47 -0400

    Add files via upload

---
+++ bin/mergeLCAs.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import argparse
+
+
+parser=argparse.ArgumentParser()
+parser.add_argument('sample_ids', nargs='+')
+parser.add_argument('files', nargs='+')
+parser.add_argument('--output', type=str, required=True)
+args=parser.parse_args()
+
+merged_df = pd.DataFrame()
+
+# Loop over each file and merge them into a single DataFrame
+for i, file_path in enumerate(args.files):
+    sample_name = args.sample_ids[i]
+    
+    # Load the lca_summary CSV
+    df = pd.read_csv(file_path)
+    
+    # Rename the 'Count' column to reflect the sample
+    df = df.rename(columns={'Count': sample_name})
+    
+    # Merge with the existing DataFrame, matching on taxonomy columns
+    if merged_df.empty:
+        merged_df = df
+    else:
+        merged_df = pd.merge(merged_df, df, on=[
+            'Root', 'Cellular Organisms', 'Superkingdom', 'Phylum', 'Class', 'Superfamily', 
+            'Family', 'Genus', 'Species'], how='outer')
+
+
+# Add a 'Total Count' column by summing across all samples
+merged_df['Total Count'] = merged_df.filter(like='Sample').sum(axis=1)
+
+sample_columns=merged_df.filter(like='Sample').columns
+merged_df[sample_columns]=merged_df[sample_columns].fillna(0).astype(int)
+merged_df['Total Count'] = merged_df['Total Count'].astype(int)
+
+for col in ['Root', 'Cellular Organisms', 'Superkingdom', 'Phylum', 'Class', 'Superfamily', 'Family', 'Genus', 'Species']:
+    merged_df[col] = merged_df[col].fillna("NA")
+
+merged_df = merged_df.sort_values(by=['Root', 'Cellular Organisms', 'Superkingdom', 'Phylum', 'Class', 'Superfamily', 'Family', 'Genus', 'Species'])
+
+merged_df.to_csv(args.output, index=False)
